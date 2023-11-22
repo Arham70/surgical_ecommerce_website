9c7858277d2608891c76ee81380617785e4d6d9d
Arham70 <nasirarham656@gmail.com>
2023-11-22 17:41:45 +0500

    create authentication system(login, signup) add additional functionality

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,47 @@
+# models.py
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    firstname = models.CharField(max_length=30, blank=True)
+    lastname = models.CharField(max_length=30, blank=True)
+    address = models.TextField(blank=True)
+    postal_code = models.CharField(max_length=10, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    telephone = models.CharField(max_length=15, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    # Set custom related_name for groups and user_permissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',
+        related_query_name='custom_user_group',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',
+        related_query_name='custom_user_permission',
+        blank=True,
+        help_text='Specific permissions for this user.',
+    )
--- authentication/views.py
+++ authentication/views.py
@@ -1,38 +1,76 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+# myapp/views.py
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import CustomUser
 
 
 def SignUpPage(request):
-    if request.method =='POST':
+    if request.method == 'POST':
         uname = request.POST.get('username')
         email = request.POST.get('email')
         pass1 = request.POST.get('password1')
         pass2 = request.POST.get('password2')
 
         if pass1 != pass2:
-            return HttpResponse("Your Password and confirm password does not match!!")
+            return HttpResponse("Your Password and confirm password do not match!!")
         else:
-            my_user = User.objects.create_user(uname,email,pass1)
+            # Create a user without the additional attributes
+            my_user = CustomUser.objects.create_user(username=uname, email=email, password=pass1)
             my_user.save()
+
+            # Redirect to profile setup page with user ID in the URL
             return redirect('login')
-    return render(request,'signup.html')
+
+    return render(request, 'signup.html')
+
+
+def ProfileSetup(request, user_id):
+    # Retrieve the user object
+    user = CustomUser.objects.get(id=user_id)
+
+    if request.method == 'POST':
+        # Get additional attributes from the form
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        address = request.POST.get('address')
+        postal_code = request.POST.get('postal_code')
+        country = request.POST.get('country')
+        telephone = request.POST.get('telephone')
+
+        # Update the user with additional attributes
+        user.firstname = firstname
+        user.lastname = lastname
+        user.address = address
+        user.postal_code = postal_code
+        user.country = country
+        user.telephone = telephone
+        user.save()
+
+        return redirect('login')  # Redirect to login or another page
+
+    return render(request, 'profile_setup.html', {'user': user})
+
 
 def LoginPage(request):
-    if request.method=='POST':
-        username=request.POST.get('username')
-        pass1=request.POST.get('password')
-        user = authenticate(request,username=username,password=pass1)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
         if user is not None:
-            login(request,user)
-            return redirect('signup')
+            login(request, user)
+            return redirect('signup')  # Redirect to user profile or another page
         else:
             return HttpResponse("Username or Password is incorrect!!")
 
-    return render(request,'login.html')
+    return render(request, 'login.html')
+
+
+@login_required
+def UserProfile(request):
+    return render(request, 'user_profile.html')
 
 
 def LogoutPage(request):
