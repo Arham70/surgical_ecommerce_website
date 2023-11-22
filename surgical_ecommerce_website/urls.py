eb8202ece7b55324738a54492d317a975057bf5d
Arham70 <nasirarham656@gmail.com>
2023-11-22 11:02:11 +0500

    create authentication system(login, signup)

--- surgical_ecommerce_website/urls.py
+++ surgical_ecommerce_website/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', views.SignUpPage, name='signup'),
+    path('login/', views.LoginPage, name='login'),
 ]
